528f2b3d903871408c6379a902cf71384890e743
Kev Bonett <basher@madasafish.com>
2022-03-17 09:44:39 +0000

    tidy ui-init comments

--- src/ts/ui-init.ts
+++ src/ts/ui-init.ts
@@ -20,7 +20,6 @@ import { uiInitTheme } from './ui-init-theme';
 
 // Instantiate all UI component modules, and do other DOM manipulation.
 export const uiInit = (): void => {
-    // TODO: consider removing post-css logical if browser usage stats show that most users are using a modern browser
     // 'postcss-logical' in conjunction with 'postcss-dir-pseudo-class' auto-generates [dir="ltr"] and [dir="rtl"] selectors in the compiled CSS. These act as fallbacks for older browsers that don't understand CSS logical properties. If <html dir="rtl"> has not been explicitly set, ensure that we programatically set default direction 'ltr' so that [dir="ltr"] CSS selector fires for older browsers:
     document.documentElement.dir = document.documentElement.dir || 'ltr';
 
@@ -35,28 +34,14 @@ export const uiInit = (): void => {
     FormValidate.start();
     RangeInput.start();
 
-    // Modal.
+    // The rest...
     Modal.start();
-
-    // Navigation.
     Navigation.start();
-
-    // Product cards.
     ProductCard.start();
-
-    // Search results filters.
     SearchFilters.start();
-
-    // Share API.
     Share.start();
-
-    // Slider.
     Slider.start();
-
-    // Sticky (header|footer) positioning.
     StickyPosition.start();
-
-    // Video player.
     VideoPlayer.start();
 
     // Dynamically import & instantiate theme-specific components.
