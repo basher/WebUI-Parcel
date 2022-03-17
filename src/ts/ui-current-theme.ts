9a4067d1bf0eb5e7e0ee742d2230d20e2e1587bf
Kev Bonett <basher@madasafish.com>
2022-03-17 08:52:26 +0000

    tidy theme overrides

--- src/ts/ui-current-theme.ts
+++ src/ts/ui-current-theme.ts
@@ -3,4 +3,4 @@ DO NOT EDIT 'ui-current-theme.ts' directly.
 
 Instead, make changes to 'ui-current-theme.ts.template', which will then be copied to 'ui-current-theme.ts' by Parcel bundler.
 */
-export const currentUITheme = 'whitelabel';
+export const currentUITheme = 'theme1';
