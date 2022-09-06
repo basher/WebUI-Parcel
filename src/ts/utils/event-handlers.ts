ad57e90374f48f7472c06f39c63035f64ae23854
Kev Bonett <basher@madasafish.com>
2022-09-06 12:11:25 +0100

    remove commented out code

--- src/ts/utils/event-handlers.ts
+++ src/ts/utils/event-handlers.ts
@@ -67,7 +67,6 @@ export const bindTabsKeyboardEvent = (
     // Stop page jumping with certain key events with 'e.preventDefault()'.
     switch (e.code) {
         case 'ArrowRight':
-            // case 'ArrowDown':
             e.preventDefault();
             if (tabButtons.length - index > 1) {
                 nextTab[index + 1].focus();
@@ -75,7 +74,6 @@ export const bindTabsKeyboardEvent = (
             }
             break;
         case 'ArrowLeft':
-            // case 'ArrowUp':
             e.preventDefault();
             if (index > 0) {
                 nextTab[index - 1].focus();
